db4ab85ac0db1c0bc750f126ef11bef434261a0a
Allan Fantoni <allan.fantoni@outlook.com>
2020-02-16 18:09:09 -0300

    exercise 2-3

--- scripts/script.js
+++ scripts/script.js
@@ -25,3 +25,26 @@ function includeHTML() {
     }
   }
 }
+
+const modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+  card.addEventListener('click', function() {
+    const content = card.getAttribute('id');
+    modalOverlay.classList.add('active');
+    modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${content}`;
+  });
+}
+
+const closeModal = document.querySelector('.close-modal').addEventListener('click', function() {
+  modalOverlay.classList.remove('active');
+  modalOverlay.querySelector('iframe').src = ``;
+});
+
+const maximizeModal = document.querySelector('.modal').addEventListener('click', function() {
+  if (modal.classList.contains('maximize')) {
+    // modal
+  }  
+});
